760a8b1dd0fa7de8891be54cb12434cb8869d125
kurisu-seima <kurisuseima@gmail.com>
2021-08-05 20:33:10 +0900

    swiftyAlert作成

---
+++ README.md
@@ -0,0 +1,5 @@
+# SwiftyAlert
+
+A description of this package.
+
+アラート表示を簡単にするライブラリです🙈
---
+++ Sources/SwiftyAlert/SwiftyAlert.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by 栗須星舞 on 2021/08/05.
+//
+
+import Foundation
+import UIKit
+
+public class AlertViewController: UIViewController {
+    
+    public func showAlert(title: String, message: String?, style: UIAlertController.Style, actions: [UIAlertAction]) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        actions.forEach { alert.addAction($0) }
+        present(alert, animated: true, completion: nil)
+    }
+}
